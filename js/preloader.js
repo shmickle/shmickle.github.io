e8bb26ef9a2e61d80fb46072ab0a43d91056a9b0
shmickle <eric.bryant0@gmail.com>
2018-01-10 19:15:29 -0500

    Edit to preloader

--- js/preloader.js
+++ js/preloader.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
         window.setTimeout(function() {
-            $("#preloader-wrap").fadeOut();
+            $("#preloader-wrap").fadeOut(400, function() {
+                $(this).remove();
+            });
         }, 1000);
 });
