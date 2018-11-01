d0c0052d22d6dc2a473168e5c1e16548b9eb076c
Eric Bryant <eric.bryant0@gmail.com>
2018-10-31 20:24:14 -0400

    Added tooltips to icons

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     sidenavInitialization();
+    tooltipSlideIn();
 }
 
 function sidenavInitialization() {
@@ -7,4 +8,22 @@ function sidenavInitialization() {
     const instances = M.Sidenav.init(elems, {
         //Sidenav options
     });
+}
+
+function tooltipSlideIn() {
+    const links = Array.from(document.querySelectorAll('.personal-links a'));
+    links
+        .map(link => link.addEventListener('mouseenter', () => {
+            link.classList.add('display');
+            setTimeout(function () {
+                link.classList.add('opacity');
+            }, 150);
+        }));
+    links
+        .map(link => link.addEventListener('mouseleave', () => {
+            link.classList.remove('opacity');
+            setTimeout(function () {
+                link.classList.remove('display');
+            }, 300);
+        }));
 }
