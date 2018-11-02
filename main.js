2d5cd597c597649ade4c783c2563ca1c601ca84e
Eric Bryant <eric.bryant0@gmail.com>
2018-11-01 21:01:18 -0400

    animated tooltips

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 window.onload = () => {
     sidenavInitialization();
-    tooltipSlideIn();
 }
 
 function sidenavInitialization() {
@@ -8,22 +7,4 @@ function sidenavInitialization() {
     const instances = M.Sidenav.init(elems, {
         //Sidenav options
     });
-}
-
-function tooltipSlideIn() {
-    const links = Array.from(document.querySelectorAll('.personal-links a'));
-    links
-        .map(link => link.addEventListener('mouseenter', () => {
-            link.classList.add('display');
-            setTimeout(function () {
-                link.classList.add('opacity');
-            }, 150);
-        }));
-    links
-        .map(link => link.addEventListener('mouseleave', () => {
-            link.classList.remove('opacity');
-            setTimeout(function () {
-                link.classList.remove('display');
-            }, 300);
-        }));
 }
